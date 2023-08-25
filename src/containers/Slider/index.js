b131298d60fadfba080e7547b90292ed6c32977a
Léo Lecée <leo.lecee@gmail.com>
2023-08-25 14:47:11 +0700

    README

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,73 +5,78 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
-	const { data } = useData();
-	const [index, setIndex] = useState(0);
-	const [isPaused, setIsPaused] = useState(false);
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
 
-	const byDateDesc = data?.focus.sort((evtA, evtB) =>
-		new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-	);
-	const nextCard = () => {
-		setIndex(index < byDateDesc.length - 1 ? index + 1 : 0);
-	};
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  );
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (!isPaused) {
-				nextCard();
-			}
-		}, 5000);
+  const nextCard = () => {
+    setIndex(index < byDateDesc.length - 1 ? index + 1 : 0);
+  };
 
-		const handleKeyDown = (e) => {
-			if (e.key === " ") {
-				setIsPaused(!isPaused);
-			}
-		};
+  const handleKeyDown = (e) => {
+    if (e.key === " ") {
+      setIsPaused(!isPaused);
+    }
+  };
 
-		document.addEventListener("keydown", handleKeyDown);
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isPaused]);
 
-		return () => {
-			clearInterval(interval);
-			document.removeEventListener("keydown", handleKeyDown);
-		};
-	});
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isPaused) {
+        nextCard();
+      }
+    }, 5000);
 
-	return (
-		<div className="SlideCardList">
-			{byDateDesc?.map((event, idx) => (
-				<div key={`slide-${event.title}`}>
-					<div
-						className={`SlideCard SlideCard--${
-							index === idx ? "display" : "hide"
-						}`}
-					>
-						<img src={event.cover} alt="forum" />
-						<div className="SlideCard__descriptionContainer">
-							<div className="SlideCard__description">
-								<h3>{event.title}</h3>
-								<p>{event.description}</p>
-								<div>{getMonth(new Date(event.date))}</div>
-							</div>
-						</div>
-					</div>
-					<div className="SlideCard__paginationContainer">
-						<div className="SlideCard__pagination">
-							{byDateDesc.map((ev, radioIdx) => (
-								<input
-									key={`radio-${ev.title}`}
-									type="radio"
-									name="radio-button"
-									checked={index === radioIdx}
-									readOnly
-								/>
-							))}
-						</div>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isPaused, nextCard]);
+
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        <div key={`slide-${event.title}`}>
+          <div
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((ev, radioIdx) => (
+                <input
+                  key={`radio-${ev.title}`}
+                  type="radio"
+                  name="radio-button"
+                  checked={index === radioIdx}
+                  readOnly
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Slider;
--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -1,56 +1,56 @@
 import PropTypes from "prop-types";
 import {
-	createContext,
-	useCallback,
-	useContext,
-	useEffect,
-	useState,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
 } from "react";
 
 const DataContext = createContext({});
 
 export const api = {
-	loadData: async () => {
-		const json = await fetch("/events.json");
-		return json.json();
-	},
+  loadData: async () => {
+    const json = await fetch("/events.json");
+    return json.json();
+  },
 };
 
 export const DataProvider = ({ children }) => {
-	const [error, setError] = useState(null);
-	const [data, setData] = useState(null);
-	const [lastEvent, setLastEvent] = useState(null);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [lastEvent, setLastEvent] = useState(null);
 
-	const getData = useCallback(async () => {
-		try {
-			const newData = await api.loadData();
-			setData(newData);
-			setLastEvent(newData.events[0]);
-		} catch (err) {
-			setError(err);
-		}
-	}, []);
+  const getData = useCallback(async () => {
+    try {
+      const newData = await api.loadData();
+      setData(newData);
+      setLastEvent(newData.events[0]);
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
 
-	useEffect(() => {
-		getData();
-	}, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-	return (
-		<DataContext.Provider
-			// eslint-disable-next-line react/jsx-no-constructed-context-values
-			value={{
-				data,
-				error,
-				lastEvent,
-			}}
-		>
-			{children}
-		</DataContext.Provider>
-	);
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+        lastEvent,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
 };
 
 DataProvider.propTypes = {
-	children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export const useData = () => useContext(DataContext);
